9da5a0ed70e2873708f1a03917edf3f9bde775c5
Kriti Kalpa <krtklpsh@gmail.com>
2020-11-08 13:31:58 +0530

    [Kriti] Add. Cheap hotel with best rating for reward customer

---
+++ HotelReservationSystem/HotelException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystem
+{
+    class HotelException : Exception
+    {
+        public enum ExceptionType
+        {
+            INVALID_CUSTOMER_TYPE,
+            INVALID_DATE
+        }
+
+        ExceptionType type;
+
+        public HotelException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
--- HotelReservationSystem/Program.cs
+++ HotelReservationSystem/Program.cs
@@ -8,16 +8,17 @@ namespace HotelReservationSystem
         {
             Console.WriteLine("Welcome to Hotel Reservation program");
 
-            //HotelManagement hotelManagementNormal = new HotelManagement(CustomerType.NORMAL);
-            //hotelManagementNormal.addHotel();
-            //hotelManagementNormal.viewHotels();
-            //string cheapestHotel = hotelManagementNormal.findCheapestHotel();
-            //Console.WriteLine("Cheapest Hotel : " + cheapestHotel);
-            //hotelManagementNormal.findCheapTotalPrice();
-            //hotelManagementNormal.bestRatedHotelForDateRange();
+            HotelManagement hotelManagementNormal = new HotelManagement(CustomerType.NORMAL);
+            hotelManagementNormal.addHotel();
+            hotelManagementNormal.viewHotels();
+            string cheapestHotel = hotelManagementNormal.findCheapestHotel();
+            Console.WriteLine("Cheapest Hotel : " + cheapestHotel);
+            hotelManagementNormal.findCheapTotalPrice();
+            hotelManagementNormal.bestRatedHotelForDateRange();
             HotelManagement hotelManagementReward = new HotelManagement(CustomerType.REWARD);
             hotelManagementReward.addHotel();
             hotelManagementReward.viewHotels();
+            hotelManagementReward.findCheapTotalPrice();
         }
     }
 }
